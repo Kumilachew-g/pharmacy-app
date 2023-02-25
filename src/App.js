a27ecbb6d244c3ca3a520c5699520e73cea9b7c5
kumilachew-g <kumilachew1212@gmail.com>
2023-02-25 17:02:08 +0300

    Add login route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-function App() {
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./routes/Login";
+
+const App = () => {
   return (
-    <div className="App">
-      <h1>Pharmacy App</h1>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
