47442899b6f57ac0f235e94804a30c2618af0e77
kumilachew-g <kumilachew1212@gmail.com>
2023-03-17 13:52:34 +0300

    Add footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import "../index.css";
+
+const Footer = () => {
+  return (
+    <div className="w-full flex">
+      <div className="w-full max-w-md m-auto bg-black py-5 px-10"></div>
+    </div>
+  );
+};
+
+export default Footer;
