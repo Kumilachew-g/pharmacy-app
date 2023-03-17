e917bebbb508da0448376b95eea6f5e64935c487
kumilachew-g <kumilachew1212@gmail.com>
2023-03-17 14:16:26 +0300

    Add Dashboard page into app to route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./routes/Login";
 import ForgotPassword from "./routes/Forgot";
 import Signup from "./routes/signup";
+import Dashboard from "./routes/Dashboard";
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
           <Route path="/" element={<Login />} />
           <Route path="forgot" element={<ForgotPassword />} />
           <Route path="signup" element={<Signup />} />
+          <Route path="dashboard" element={<Dashboard />} />
         </Routes>
       </Router>
     </div>
