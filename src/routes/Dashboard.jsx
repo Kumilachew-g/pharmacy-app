d2bcfd8cbdb4d9bf1254526490aa6c989e41885b
kumilachew-g <kumilachew1212@gmail.com>
2023-03-17 14:15:37 +0300

    Switch logo icon with google icon

--- src/routes/Dashboard.jsx
+++ src/routes/Dashboard.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import "../index.css";
-import logo from "../assets/logo.svg";
+import google from "../assets/google.svg";
 
 const Dashboard = () => {
   return (
     <div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
       <div class="flex-shrink-0">
-        <img class="h-12 w-12" src={logo} alt="ChitChat Logo" />
+        <img class="h-12 w-12" src={google} alt="ChitChat Logo" />
       </div>
       <div>
         <div class="text-xl font-medium text-black">ChitChat</div>
