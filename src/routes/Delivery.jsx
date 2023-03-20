af2cc682c7aafc8aee320dc1acc46e54d89f6656
kumilachew-g <kumilachew1212@gmail.com>
2023-03-20 12:59:17 +0300

    Update the path

--- src/routes/Delivery.jsx
+++ src/routes/Delivery.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "../index.css";
-import Header from "../components/header";
-import SideBar from "../components/sideBar";
+import Header from "../components/Header";
+import SideBar from "../components/SideBar";
 
 const Delivery = () => {
   return (
