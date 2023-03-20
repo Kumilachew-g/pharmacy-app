6d1a6165d4951417ef5a41beda7abb86f0a35185
kumilachew-g <kumilachew1212@gmail.com>
2023-03-20 12:54:21 +0300

    Add delivery page

---
+++ src/routes/Delivery.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "../index.css";
+import Header from "../components/header";
+import SideBar from "../components/sideBar";
+
+const Delivery = () => {
+  return (
+    <div>
+      <Header />
+      {/* sidebar */}
+      <SideBar />
+      {/* main content container */}
+      <div></div>
+    </div>
+  );
+};
+
+export default Delivery;
