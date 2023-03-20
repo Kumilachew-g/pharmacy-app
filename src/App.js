3aa962e9230298106367625a295d7a5742e4b918
kumilachew-g <kumilachew1212@gmail.com>
2023-03-20 13:23:56 +0300

    Update import file paths

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./routes/Login";
 import ForgotPassword from "./routes/Forgot";
-import Signup from "./routes/Signup";
+import Signup from "./routes/SignUp";
 import Dashboard from "./routes/Dashboard";
 import Homepage from "./routes/LandingPage";
 import Profile from "./routes/Profile";
