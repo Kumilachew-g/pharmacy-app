c4506ae81587e66e1d70abecc46e2fa6c8308817
kumilachew-g <kumilachew1212@gmail.com>
2023-03-20 12:46:00 +0300

    Add Billing page

---
+++ src/routes/Billing.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "../index.css";
+import Header from "../components/Header";
+import SideBar from "../components/SideBar";
+
+const Billing = () => {
+  return (
+    <div>
+      <Header />
+      {/* sidebar */}
+      <SideBar />
+      {/* main content container */}
+      <div></div>
+    </div>
+  );
+};
+
+export default Billing;
