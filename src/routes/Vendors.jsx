770d12215c3dd76f3935425c005edad823bb1827
kumilachew-g <kumilachew1212@gmail.com>
2023-03-20 12:56:47 +0300

    Add vendors page

---
+++ src/routes/Vendors.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "../index.css";
+import Header from "../components/Header";
+import SideBar from "../components/sideBar";
+
+const Vendor = () => {
+  return (
+    <div>
+      <Header />
+      {/* sidebar */}
+      <SideBar />
+      {/* main content container */}
+      <div></div>
+    </div>
+  );
+};
+
+export default Vendor;
