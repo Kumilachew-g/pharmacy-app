28da6287fae03ad49358a1f187ddc03af3b0737a
Kumilachew-g <kumilachew1212@gmail.com>
2023-03-26 22:40:41 +0300

    Update tailwindCss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     fontFamily: {
@@ -8,37 +8,37 @@ module.exports = {
     },
     extend: {
       screens: {
-        sm: "640px",
+        sm: '640px',
         // => @media (min-width: 640px) { ... }
 
-        md: "768px",
+        md: '768px',
         // => @media (min-width: 768px) { ... }
 
-        lg: "1024px",
+        lg: '1024px',
         // => @media (min-width: 1024px) { ... }
 
-        xl: "1280px",
+        xl: '1280px',
         // => @media (min-width: 1280px) { ... }
 
-        "2xl": "1536px",
+        '2xl': '1536px',
         // => @media (min-width: 1536px) { ... }
       },
       colors: {
-        primary: "#5E48E8",
-        secondary: "#489BE8",
-        tertiary: "#F3D9DA",
-        success: "#ED4B9E",
-        error: "#ED4B9E",
-        dark: "#0E0E2C",
-        text: "#111111",
-        accent: "#ECF1F4",
-        light: "#FAFCFE",
-        subtle: "#8C8CA1",
+        primary: '#5E48E8',
+        secondary: '#489BE8',
+        tertiary: '#F3D9DA',
+        success: '#ED4B9E',
+        error: '#ED4B9E',
+        dark: '#0E0E2C',
+        text: '#111111',
+        accent: '#ECF1F4',
+        light: '#FAFCFE',
+        subtle: '#8C8CA1',
       },
     },
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/forms')],
 };
