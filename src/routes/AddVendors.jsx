14786465ec2899a0a950166631dc8512aad57837
Kumilachew-g <kumilachew1212@gmail.com>
2023-03-26 22:34:04 +0300

    Add AddVendors pages with styling

---
+++ src/routes/AddVendors.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../../index.css';
+import Header from '../components/Header';
+import Sidebar from '../components/SideBar';
+
+const AddVendor = () => {
+  return (
+    <div>
+      <Header />
+      {/* sidebar */}
+      <div className='h-9/10 '>
+        <Sidebar />
+        {/* main content container */}
+      </div>
+    </div>
+  );
+};
+
+export default AddVendor;
