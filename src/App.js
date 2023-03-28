20ecda85f0afc894bbf27e2becbd2412c7aa4885
Kumilachew-g <kumilachew1212@gmail.com>
2023-03-28 19:51:22 +0300

    Fix login page

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ const App = () => {
       <Router>
         <Routes>
           <Route path='/' element={<Login />} />
+          <Route path='/login' element={<Login />} />
           <Route path='/forgot' element={<ForgotPassword />} />
           <Route path='/signup' element={<Signup />} />
           <Route path='/home' element={<Homepage />} />
