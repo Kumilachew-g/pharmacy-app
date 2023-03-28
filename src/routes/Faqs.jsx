6bbc36dd3c9ce0e5616ba6c4ccc7e9bc7ded3892
Kumilachew-g <kumilachew1212@gmail.com>
2023-03-28 20:12:25 +0300

    Auto fix quotes

--- src/routes/Faqs.jsx
+++ src/routes/Faqs.jsx
@@ -1,27 +1,27 @@
-import React from "react";
-import "../index.css";
-import Header from "../components/HeaderAdvance";
-import { ChevronUpIcon } from "@heroicons/react/solid";
+import React from 'react';
+import '../index.css';
+import Header from '../components/HeaderAdvance';
+import { ChevronUpIcon } from '@heroicons/react/solid';
 // import MyDisclosure from "../components/disclosure";
-import { Disclosure, Transition } from "@headlessui/react";
+import { Disclosure, Transition } from '@headlessui/react';
 
 function MyDisclosure() {
   return (
-    <div className="">
+    <div className=''>
       <Disclosure>
         {({ open }) => (
-          <div className="container shadow bg-white rounded-lg my-3">
-            <Disclosure.Button className="w-full flex justify-between p-4 text-2xl text-left text-black  focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
-              <span className={`${!open ? "text-black " : "text-primary"}`}>
+          <div className='container shadow bg-white rounded-lg my-3'>
+            <Disclosure.Button className='w-full flex justify-between p-4 text-2xl text-left text-black  focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75'>
+              <span className={`${!open ? 'text-black ' : 'text-primary'}`}>
                 How does the dasboard display the data?
               </span>
               <ChevronUpIcon
                 className={`${
-                  !open ? "transform rotate-180 text-black " : "text-primary"
+                  !open ? 'transform rotate-180 text-black ' : 'text-primary'
                 } w-7 h-7 ml-auto item-end`}
               />
             </Disclosure.Button>
-            <Disclosure.Panel className="p-4 text-md text-black">
+            <Disclosure.Panel className='p-4 text-md text-black'>
               The data from the databse is used to display the data to the
               dashboard.
             </Disclosure.Panel>
@@ -34,12 +34,12 @@ function MyDisclosure() {
 
 const Faqs = () => {
   return (
-    <div className="bg-gray-50">
+    <div className='bg-gray-50'>
       <Header />
-      <div className="h-9/10 flex ">
-        <div className="justify-center mt-12 ml-auto mr-auto w-2/3">
-          <div className="">
-            <h1 className="text-center text-3xl font-medium mt-8 mb-4 antialiased p-5">
+      <div className='h-9/10 flex '>
+        <div className='justify-center mt-12 ml-auto mr-auto w-2/3'>
+          <div className=''>
+            <h1 className='text-center text-3xl font-medium mt-8 mb-4 antialiased p-5'>
               Have any question?
             </h1>
           </div>
