53afcd7cd04509f89819c3a2602793812ed451db
kumilachew-g <kumilachew1212@gmail.com>
2023-03-29 17:50:53 +0300

    Refactor AddVendors page

--- src/routes/AddVendors.jsx
+++ src/routes/AddVendors.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import '../../index.css';
-import Header from '../components/Header';
-import Sidebar from '../components/SideBar';
+import React from "react";
+import "../index.css";
+import Header from "../components/Header";
+import Sidebar from "../components/SideBar";
 
 const AddVendor = () => {
   return (
     <div>
       <Header />
       {/* sidebar */}
-      <div className='h-9/10 '>
+      <div className="h-9/10 ">
         <Sidebar />
         {/* main content container */}
       </div>
