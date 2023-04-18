a4b72506acaccadcbd44f3fcf1988b4a9f024525
kumilachew-g <kumilachew1212@gmail.com>
2023-04-18 16:50:00 +0300

    Connect vendor redux for action

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,5 +1,8 @@
 // Import all actions here!
 import * as AuthActions from "./auth.action";
 
+import * as ProductTypes from "./product.action";
+import * as VendorTypes from "./vendor.actions";
+
 // Export all actions here!
-export { AuthActions };
+export { AuthActions, ProductTypes, VendorTypes };
