88daa3c925d66f9d5a6a2307a447715e6bedfa58
kumilachew-g <kumilachew1212@gmail.com>
2023-04-28 18:27:00 +0300

    Refactor Loader page

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,16 +1,11 @@
-import * as Loader from "react-loader-spinner";
+import Loader from "react-loader-spinner";
 import React, { Component } from "react";
 
 class Loading extends Component {
   //other logic
   render() {
     return (
-      <Loader
-        type="TailSpin"
-        color={this.props.color ? this.props.color : "#000000"}
-        height={this.props.size ? this.props.size : 30}
-        width={this.props.size ? this.props.size : 30}
-      />
+      <Loader type="TailSpin" color={this.props.color?this.props.color:"#000000"} height={this.props.size?this.props.size:30} width={this.props.size?this.props.size:30} />
     );
   }
 }
