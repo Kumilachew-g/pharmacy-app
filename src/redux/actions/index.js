2cd269539f65a5e22365895d723bbfadaab7acaa
kumilachew-g <kumilachew1212@gmail.com>
2023-04-29 10:36:20 +0300

    Update Index page and is't functions

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,8 +1,9 @@
 // Import all actions here!
 import * as AuthActions from "./auth.action";
-
-import * as ProductTypes from "./product.action";
-import * as VendorTypes from "./vendor.actions";
-
+import * as ProductActions from "./product.action";
+import * as VendorActions from "./vendor.actions";
+import * as HomeActions from "./home.action";
+import * as UserActions from "./user.action";
+import * as BillActions from "./bill.action";
 // Export all actions here!
-export { AuthActions, ProductTypes, VendorTypes };
+export { AuthActions, ProductActions, VendorActions, HomeActions, UserActions,BillActions };
